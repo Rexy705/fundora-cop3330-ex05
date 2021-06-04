c29049ade910b607564a505179c0ef0d7fa18733
Rey <reyfundora@icloud.com>
2021-06-03 21:50:28 -0400

    Complete.

--- src/main/java/ex05/ex05.java
+++ src/main/java/ex05/ex05.java
@@ -1,14 +1,15 @@
+package ex05;
+
 import java.util.Scanner;
 
 public class ex05 {
     public static void main (String[] args) {
-        Scanner input1 = new Scanner(System.in),
-                input2 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("What is the first number? ");
-        String s1 = input1.next();
+        String s1 = input.next();
         System.out.print("What is the second number? ");
-        String s2 = input2.next();
+        String s2 = input.next();
 
         int first = Integer.parseInt(s1);
         int second = Integer.parseInt(s2);
@@ -19,8 +20,8 @@ public class ex05 {
         int total4 = first / second;
 
         System.out.println(first + " + " + second + " = " + total1 + "\n" +
-                           first + " - " + second + " = " + total2 + "\n" +
-                           first + " * " + second + " = " + total3 + "\n" +
-                           first + " / " + second + " = " + total4);
-        }
+                first + " - " + second + " = " + total2 + "\n" +
+                first + " * " + second + " = " + total3 + "\n" +
+                first + " / " + second + " = " + total4);
     }
+}
